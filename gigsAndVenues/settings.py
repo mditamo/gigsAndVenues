cc787e094f56f4f782ac1f816473cb8bd79e8155
Matias <mditamo@gmail.com>
2012-05-30 17:24:38 -0300

    Se agrega configuracion de la base de datos

--- gigsAndVenues/settings.py
+++ gigsAndVenues/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.oracle', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '127.0.0.1/XE',                      # Or path to database file if using sqlite3.
+        'USER': 'proyecto',                      # Not used with sqlite3.
+        'PASSWORD': 'proyecto',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '',# Set to empty string for default. Not used with sqlite3.
     }
 }
 
