7c9e1af723c6cf5f5943e07f1f4d6ea04cb5a944
Lucas Paoliello <paoliellolucas@gmail.com>
2012-10-06 19:03:45 -0300

    Cambios negociaciones sin sugerencia fechas ni repeticion de carga de condiciones.

---
+++ condiciones/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from condiciones.models import Tipo_Condicion
+
+admin.site.register(Tipo_Condicion)
---
+++ condiciones/views.py
@@ -0,0 +1,23 @@
+from django.template.context import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from usuario.models import UsuarioRegistrado
+from negociacion.models import CondicionNegociacion
+from condiciones.forms import CondicionUnitariaForm
+
+def agregarCondicion(request,negociacion_id):
+    usuario_registrado=UsuarioRegistrado.objects.get(pk=request.user.id)
+    if request.method == 'POST':
+        form = CondicionUnitariaForm(request.POST)              
+        condicion=form.save(commit=False)      
+        if form.is_valid():
+            condicion.save()
+            condicionNegociacion = CondicionNegociacion.objects.create(negociacion_id=negociacion_id, condicionUnitaria_id=condicion.id)
+            condicionNegociacion.save()
+        return HttpResponseRedirect(reverse('negociacion.views.listado'))
+        #return HttpResponseRedirect(reverse('negociacion.views.negociacionPaso4', kwargs={'negociacion_id':negociacion.id}))
+    else:
+        form = CondicionUnitariaForm()
+        negociacion_id = negociacion_id
+        return render_to_response("negociacion/nuevo4.html", locals(), context_instance=RequestContext(request))
